f71aba8dcfc96a07c777e171b668596fbe951dd9
john0829 <joh860829nny@gmail.com>
2017-12-17 23:45:07 +0800

    tmep

---
+++ public/js/ChatReceive.js
@@ -0,0 +1,58 @@
+
+var socket = io.connect();
+
+$(document).ready(function() {
+	var chatApp = new Chat(socket);
+	socket.emit('rooms');
+
+	socket.on('nameResult', function(result) {
+	    var message;
+
+	    if (result.success) 
+	    {
+	    	message = 'You are now known as ' + result.name + '.';
+	    } 
+	    else 
+	    {
+	    	message = decryptMessage(result.message);
+	    }
+	    $('#messages').append(divSystemContentElement(message));
+	});
+
+	socket.on('joinResult', function(result) {
+    	$('#room').text(result.room);
+    	$('#messages').append(divSystemContentElement('Room changed.'));
+	});
+
+	socket.on('BroadCastmessage', function (message) {
+		message.text = decryptMessage(message.text);
+    	var newElement = $('<div></div>').text(message.text);
+    	$('#messages').append(newElement);
+    	showWindows(message.text);
+    });
+
+	socket.on('message', function (message) {
+		message.text = decryptMessage(message.text);
+    	addRoom(message.room);
+    	var newElement = $('<div></div>').text(message.text);
+    	$('#messages').append(newElement);  
+	});
+
+	$('#send-message').focus();
+
+	socket.on('rooms', function(rooms) {
+		for(var room in rooms) 
+    	{
+    		room = room.substring(1, room.length);
+    		if (room != '') 
+    		{
+    			$('#room-list').append(divAddIdContentElement(room));
+    		}
+    	}
+
+    	$('#room-list div').click(function() {
+    		chatApp.processCommand('/join ' + $(this).text());
+    		$('#send-message').focus();
+    	});
+	});
+});
---
+++ public/js/Encrypt.js
@@ -0,0 +1,62 @@
+function encrypt(text){
+	var key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	// 將文字轉換為位元組
+	var textBytes = aesjs.utils.utf8.toBytes(text);
+
+	// Counter 可省略，若省略則從 1 開始
+	var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+	var encryptedBytes = aesCtr.encrypt(textBytes);
+
+	// 加密過後的資料是二進位資料，若要輸出可轉為十六進位格式
+	var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+	console.log(encryptedHex);
+
+	// 將十六進位的資料轉回二進位
+	var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+
+	// 解密時要建立另一個 Counter 實體
+	var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+	var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+
+	// 將二進位資料轉換回文字
+	var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+	console.log(decryptedText);
+	return encryptedHex;
+}
+
+function decryptMessage(text){
+	var key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+	var encryptedBytes = aesjs.utils.hex.toBytes(text);
+
+	// 解密時要建立另一個 Counter 實體
+	var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+	console.log(text);
+	console.log(encryptedBytes);
+	var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+
+	// // 將二進位資料轉換回文字
+	var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+	console.log(decryptedText);
+	return decryptedText;
+}
+
+function getKey(){
+	// The passphrase used to repeatably generate this RSA key.
+	var PassPhrase = "The Moon is a Harsh Mistress."; 
+
+	// The length of the RSA key, in bits.
+	var Bits = 1024; 
+
+	var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
+	//console.log(MattsRSAkey);
+	var MattsPublicKeyString = cryptico.publicKeyString(MattsRSAkey);  
+	console.log(MattsPublicKeyString);
+
+	var PlainText = "Matt";
+	var EncryptionResult = cryptico.encrypt(PlainText, MattsPublicKeyString);
+	console.log(EncryptionResult.cipher);
+	console.log(MattsRSAkey);
+	var DecryptionResult = cryptico.decrypt(EncryptionResult.cipher, MattsRSAkey);
+	console.log(DecryptionResult.plaintext);
+	return(MattsRSAkey);
+}
